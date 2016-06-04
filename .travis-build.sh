9d39955446d5834cf10f8414fe9320ccbb538c05
jsantino <jsantino@cs.washington.edu>
2016-06-03 17:25:02 -0700

    change to use indexchecker

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,12 +5,12 @@ ROOT=$TRAVIS_BUILD_DIR/..
 set -e
 
 ## Build Checker Framework
-(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+(cd $ROOT && git clone https://github.com/gyhughes/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
 (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 ## Obtain plume-lib
-(cd $ROOT && git clone https://github.com/mernst/plume-lib.git)
+(cd $ROOT && git clone https://github.com/plume-lib-check-index.git)
 
-make -C $ROOT/plume-lib/java check-types
+make -C $ROOT/plume-lib/java JAVACHECK_ARGS="-processor org.checkerframework.checker.index.IndexChecker" check-types
