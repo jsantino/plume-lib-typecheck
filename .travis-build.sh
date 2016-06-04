7739447bc64fbe1f19534a27af02a8fce5d829f3
jsantino <jsantino@cs.washington.edu>
2016-06-03 17:45:43 -0700

    fix repo name

--- .travis-build.sh
+++ .travis-build.sh
@@ -11,7 +11,7 @@ set -e
 export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 ## Obtain plume-lib
-(cd $ROOT && git clone https://github.com/plume-lib-check-index.git)
+(cd $ROOT && git clone https://github.com/jsantino/plume-lib-check-index.git)
 
 make -C $ROOT/plume-lib/java JAVACHECK_ARGS="-source 7 -target 7 -Xlint:-options -Awarns -implicit:class -Xlint:-processing -AcheckPurityAnnotations -processor org.checkerframework.checker.index.IndexChecker" check-types
 
